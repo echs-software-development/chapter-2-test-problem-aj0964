4d74878dbdfcfe4bd5ec33b29e78ad19ccd56b34
whack <aj0964@students.necsd.net>
2020-11-12 15:01:00 +0000

    Chapter2testanswer

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -4,9 +4,9 @@ public class Chapter2Test {
   public static void main(String[] args) {
   Scanner scan = new Scanner(System.in);
   System.out.println("Enter 1st interger:");
-  int num1 = Scan.nextInt();
+  int num1 = scan.nextInt();
   System.out.println("Enter 2nd interger:");
-  int num2 = Scan.nextInt();
+  double num2 = scan.nextDouble();
   double avg = (num1 + num2) /2;
   System.out.print("Your average is: " + avg);
 
