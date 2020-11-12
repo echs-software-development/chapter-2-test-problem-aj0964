93be6aa58226f1822c166f67f9fa477d683a3a47
whack <aj0964@students.necsd.net>
2020-11-12 14:56:38 +0000

    chapter2testproblem

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,15 @@
-
+import java.util.Scanner;
 
 public class Chapter2Test {
   public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter 1st interger:");
+  int num1 = Scan.nextInt();
+  System.out.println("Enter 2nd interger:");
+  int num2 = Scan.nextInt();
+  double avg = (num1 + num2) /2;
+  System.out.print("Your average is: " + avg);
+
   
   }
 }
